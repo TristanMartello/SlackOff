d98a35824a0f30a21ede3dd3eac8ba05de15c119
TristanMartello <64500848+TristanMartello@users.noreply.github.com>
2022-02-22 15:00:14 -0500

    Game basics, preliminary player movement

---
+++ SlackOff/Assets/PlayerMove.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float moveSpeed = 5f;
+    public Vector2 movement;
+    // Start is called before the first frame update
+    void Start(){
+        rb = GetComponent<Rigidbody2D> ();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate(){
+        movement.x = Input.GetAxisRaw ("Horizontal");
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
