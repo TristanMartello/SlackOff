4235659ac885d9cb93298d1d309980ae7dd9169d
cxiong02 <93942226+cxiong02@users.noreply.github.com>
2022-03-05 18:58:40 -0500

    Revert "Changes"
    This reverts commit 1d53f019830fbbe3b4a257ddba53b397549d4664.

--- SlackOff/Assets/GameInventory.cs
+++ SlackOff/Assets/GameInventory.cs
@@ -11,11 +11,9 @@ public class GameInventory : MonoBehaviour {
     public static string currItem = "None";
 
     public static bool diskBool = false;
-    public static bool cubeBool = false;
-    //public static bool penBool = false;   
+    public static bool cubeBool = false;    
     public GameObject diskImage;
     public GameObject cubeImage;
-    //public GameObject penImage;
     
     private int arrLen = 2;
     private GameObject[] objArr;
@@ -36,11 +34,9 @@ public class GameInventory : MonoBehaviour {
         
         objArr[0] = diskImage;
         objArr[1] = cubeImage;
-        //objArr[2] =  penImage;
 
         boolArr[0] = diskBool;
         boolArr[1] = cubeBool;
-        //boolArr[2] =  penBool;
 
     }
 
