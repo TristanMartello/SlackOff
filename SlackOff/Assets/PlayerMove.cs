b290d5f1646dec6f7efe95b6085547ef243906bd
TristanMartello <64500848+TristanMartello@users.noreply.github.com>
2022-03-06 14:35:08 -0500

    Added TeddyBear and PaperStack objects

--- SlackOff/Assets/PlayerMove.cs
+++ SlackOff/Assets/PlayerMove.cs
@@ -6,7 +6,6 @@ public class PlayerMove : MonoBehaviour
 {
     public Rigidbody2D rb;
     public GameInventory playerInventory;
-    public Transform dropPoint;
     
     public GameObject currItem;
     
